816ef7c225185f565e72a8524fd2a744541e4047
Ian Verderame <ianverderame9@gmail.com>
2021-10-15 17:22:34 -0600

    initial commit: :

---
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -0,0 +1,7 @@
+package com.skilldistillery.foodtruck;
+//data: name, rating, genre of food, id
+public class FoodTruck {
+//this class has data
+	//including ID which will be generated 
+	//automatically
+}
---
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.foodtruck;
+
+public class FoodTruckApp {
+// Scanner, menu loop, until user types quit - menu will end but program
+	// continues. UP TO five trucks
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
